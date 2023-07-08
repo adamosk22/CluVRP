4c596c5c414e14d210a32226fab4524fd4fc9c63
adamosk22 <56248578+adamosk22@users.noreply.github.com>
2023-07-08 22:45:27 +0200

    fixed problem with returning to cluster start
    In the previous versions the driver had to return to the start of cluster to move to the next. Now it is possible to go from the end of cluster.

--- project.py
+++ project.py
@@ -23,28 +23,6 @@ def extract_data_from_xml(xml_file):
 
     return data
 
-def create_distance_matrix(extracted_data, number):
-    coordinates = []
-    for node in extracted_data:
-        if node['Cluster'] == number:
-            coord = (node['CoordX'], node['CoordY'])
-            coordinates.append(coord)
-    print(coordinates)
-    num_nodes = len(coordinates)
-    distance_matrix = np.zeros((num_nodes, num_nodes))
-
-    for i in range(num_nodes):
-        for j in range(num_nodes):
-            if i == j:
-                distance_matrix[i][j] = 0
-            else:
-                coord_i = coordinates[i]
-                coord_j = coordinates[j]
-                distance = calculate_distance(coord_i, coord_j)
-                distance_matrix[i][j] = distance
-
-    return distance_matrix
-
 def tsp_nearest_neighbor(coordinates, number):
     num_nodes = len(coordinates)
     visited = [False] * num_nodes
@@ -74,29 +52,19 @@ def tsp_nearest_neighbor(coordinates, number):
         visited[nearest_neighbor] = True
         current_node = nearest_neighbor
 
-    # Dodaj odległość powrotną do punktu początkowego
-    if number == -1:
-        last_node = path[-1]
-        distance_to_start = calculate_distance(coordinates[last_node], coordinates[start_node])
-        total_distance += distance_to_start
-        path.append(start_node)
 
     path_global = []
 
     for node in path:
         format_node = str(number) + "." + str(node + 1)
         path_global.append(format_node)
-
-    if number >= 0:
-        return path_global, total_distance, number, coordinates[0], coordinates[path[-1]]
-    else:
-        return path, total_distance, number, coordinates[0], coordinates[0]
+        
+    return path_global, total_distance, number, coordinates[0], coordinates[path[-1]]
     
-def tsp_nearest_neighbor_global(coordinates, number):
+def tsp_nearest_neighbor_global(coordinates):
     num_nodes = len(coordinates)
     visited = [False] * num_nodes
 
-    # Wybierz losowy punkt początkowy
     start_node = 0
     visited[start_node] = True
     current_node = start_node
@@ -110,34 +78,27 @@ def tsp_nearest_neighbor_global(coordinates, number):
 
         for neighbor_node in range(num_nodes):
             if not visited[neighbor_node]:
-                distance = calculate_distance(coordinates[current_node], coordinates[neighbor_node])
+                distance = calculate_distance(coordinates[current_node][0], coordinates[neighbor_node][0])
+                if coordinates[current_node][1] == coordinates[neighbor_node][1]:
+                    distance = 0
 
                 if distance < nearest_distance:
                     nearest_distance = distance
                     nearest_neighbor = neighbor_node
 
-        path.append(nearest_neighbor)
+        if(coordinates[nearest_neighbor][1] not in path):
+            path.append(coordinates[nearest_neighbor][1])
         total_distance += nearest_distance
         visited[nearest_neighbor] = True
         current_node = nearest_neighbor
 
     # Dodaj odległość powrotną do punktu początkowego
-    if number == -1:
-        last_node = path[-1]
-        distance_to_start = calculate_distance(coordinates[last_node], coordinates[start_node])
-        total_distance += distance_to_start
-        path.append(start_node)
+    last_node = path[-1]
+    distance_to_start = calculate_distance(coordinates[last_node][0], coordinates[start_node][0])
+    total_distance += distance_to_start
+    path.append(start_node)
 
-    path_global = []
-
-    for node in path:
-        format_node = str(number) + "." + str(node + 1)
-        path_global.append(format_node)
-
-    if number >= 0:
-        return path_global, total_distance, number, coordinates[0], coordinates[path[-1]]
-    else:
-        return path, total_distance, number, coordinates[0], coordinates[0]
+    return path, total_distance
 
 def find_path(extracted_data):
     paths = []
@@ -158,7 +119,7 @@ def find_path(extracted_data):
         base_coordinates.append([path[3], path[2]])
         base_coordinates.append([path[4], path[2]])
 
-    base_result = tsp_nearest_neighbor(base_coordinates, -1)
+    base_result = tsp_nearest_neighbor_global(base_coordinates)
     base_path = base_result[0]
     print(base_path)
     distance += base_result[1]
@@ -182,10 +143,8 @@ def calculate_distance(coord1, coord2):
     distance = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     return distance
 
-# Ścieżka do pliku XML
 xml_file_path = 'dataset.xml'
 
-# Wywołanie funkcji extract_data_from_xml
 extracted_data = extract_data_from_xml(xml_file_path)
 
 find_path(extracted_data)
